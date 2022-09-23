6066008ae6ef7fa5475f13fc61c6e8f9f226511c
sbuazjr <rwothomiosolomon1115@gmail.com>
2022-09-23 01:33:45 -0700

    printed

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,23 +1,30 @@
 #include "hash_tables.h"
+
 /**
- *hash_table_print - prints a hash table.
+ * hash_table_print - prints a hash table
+ * @ht: hash table to print
  *
- *@ht: hash table to be printed out
+ * Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned int idx, end;
+	unsigned long int i;
 	hash_node_t *tmp;
+	char flag = 0; /* 0 while no data has been printed yet */
 
-	if (!ht)
+	if (ht == NULL || ht->array == NULL)
 		return;
 	printf("{");
-	for (idx = 0, end = 1; idx < ht->size; idx++)
+	for (i = 0; i < ht->size; i++)
 	{
-		for (tmp = ht->array[idx]; tmp != NULL; tmp = tmp->next)
+		tmp = ht->array[i];
+		while (tmp != NULL)
 		{
-			printf("%s'%s': '%s'", end == 1 ? "" : ",", tmp->key, tmp->value);
-			end = 0;
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			flag = 1;
+			tmp = tmp->next;
 		}
 	}
 	printf("}\n");
